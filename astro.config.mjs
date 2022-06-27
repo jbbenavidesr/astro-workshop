789b637c4d3e0d3364eb6c17e2b57077fa2a4516
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2022-06-27 10:42:47 -0500

    :heavy_plus_sign: Add svelte for handling client side interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-workshop.netlify.app",
+  site: "https://astro-workshop.netlify.app",
+  integrations: [svelte()]
 });
